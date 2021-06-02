abcc08752e9df02c219e622ebcd0ac452367d4ad
Elsayedmostafa574 <alsayedb574@gmail.com>
2021-06-02 14:07:20 +0200

    elsayed

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 700,
+	speedAsDuration: true
+});
+
+
+$(window).scroll(function () {
+	let x = $(window).scrollTop()
+	if (x > 50) {
+		$('.navbar').css({
+			
+			'background-color': '#89a794',
+
+		})
+		$('.nav-link').css({
+			'color': 'white'
+		})
+	}
+	else {
+		$('.navbar').css({
+			
+			'background-color': 'transparent',
+
+		})
+		$('.nav-link').css({
+			'color': 'black'
+		})
+	}
+
+})
